7a91727a100449a4eb37a87a4ef985d1cb261a91
Владислав Кикоть <devy@MacBook-Pro-Vladislav.local>
2021-07-31 21:51:11 +0300

    Initila commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# java-app
+Java Command Line Application
---
+++ src/main/java/com/company/javaapp/App.java
@@ -0,0 +1,8 @@
+package com.company.javaapp;
+
+public class App {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
